b69384d7ec0405730f80eb0c3437d42b7e62372a
charmi1909 <charmibhalodiya19@gmail.com>
2026-03-06 20:57:20 +0530

    Signup

--- .angular_ecommerce/src/app/services/auth.service.ts
+++ .angular_ecommerce/src/app/services/auth.service.ts
@@ -1,3 +1,54 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthService {
+//   private apiUrl = 'http://localhost:3001'; 
+//   constructor(private http: HttpClient) {}
+
+//   signUp(userData: any): Observable<any> {
+//     return this.http.post(`${this.apiUrl}/signup`, userData);
+//   }
+
+//   signIn(credentials: any): Observable<any> {
+//     return this.http.post(`${this.apiUrl}/signin`, credentials);
+//   }
+
+//   storeUserData(token: string, userId: string): void {
+//     localStorage.setItem('token', token);
+//     localStorage.setItem('userId', userId);
+//   }
+
+//   getToken(): string | null {
+//     return localStorage.getItem('token');
+//   }
+
+//   getUserId(): string | null {
+//     return localStorage.getItem('userId');
+//   }
+
+//   isLoggedIn(): boolean {
+//     return !!this.getToken(); 
+//   }
+
+//   logout(): void {
+//     localStorage.removeItem('token');
+//     localStorage.removeItem('userId');
+//   }
+
+//   getAuthHeaders(): HttpHeaders {
+//     const token = this.getToken();
+//     return new HttpHeaders({
+//       'Content-Type': 'application/json',
+//       'Authorization': token ? `Bearer ${token}` : ''
+//     });
+//   }
+// }
+
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -6,7 +57,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3001'; 
+
+  private apiUrl = 'https://ecommerce-website-angular-3.onrender.com';
+
   constructor(private http: HttpClient) {}
 
   signUp(userData: any): Observable<any> {
@@ -31,7 +84,7 @@ export class AuthService {
   }
 
   isLoggedIn(): boolean {
-    return !!this.getToken(); 
+    return !!this.getToken();
   }
 
   logout(): void {
--- .angular_ecommerce/src/app/services/user-api.service.ts
+++ .angular_ecommerce/src/app/services/user-api.service.ts
@@ -1,26 +1,53 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable ,inject} from '@angular/core';
+// import { Product } from './product';
+// import { Observable } from 'rxjs';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UserApiService {
+
+//   api_link="http://localhost:3001"
+//   private _http= inject(HttpClient);
+
+//   getAllCategory(){
+//     return this._http.get(`${this.api_link}/category`)
+//   }
+
+//   getProductsByCategoryId(id: string): Observable<Product[]> {
+//     return this._http.get<Product[]>(`http://localhost:3001/products/category/${id}`);
+//   }
+
+//   addToCart(data: { productID: string; userID: string }) {
+//     return this._http.post<{ cart: { totalAmount: number } }>(`http://localhost:3001/carts`, data);
+//   }
+  
+  
+// }
+
 import { HttpClient } from '@angular/common/http';
-import { Injectable ,inject} from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Product } from './product';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
 
-  api_link="http://localhost:3001"
-  private _http= inject(HttpClient);
+  api_link = "https://ecommerce-website-angular-3.onrender.com";
+  private _http = inject(HttpClient);
 
   getAllCategory(){
     return this._http.get(`${this.api_link}/category`)
   }
 
   getProductsByCategoryId(id: string): Observable<Product[]> {
-    return this._http.get<Product[]>(`http://localhost:3001/products/category/${id}`);
+    return this._http.get<Product[]>(`${this.api_link}/products/category/${id}`);
   }
 
   addToCart(data: { productID: string; userID: string }) {
-    return this._http.post<{ cart: { totalAmount: number } }>(`http://localhost:3001/carts`, data);
+    return this._http.post<{ cart: { totalAmount: number } }>(`${this.api_link}/carts`, data);
   }
-  
-  
+
 }
